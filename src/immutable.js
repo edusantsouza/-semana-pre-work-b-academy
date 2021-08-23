9e7ef3c39eacc124eae7db0cf6fae24dbbdb23cb
edusantsouza <eduardo.santsouza@gmail.com>
2021-08-22 21:21:57 -0300

    Pré-Work - Dia 2

---
+++ src/immutable.js
@@ -0,0 +1,22 @@
+const john = {
+  name: 'John',
+  surname: 'Doe',
+  age: 30,
+  hobbies: ['Surf', 'Design'],
+}
+
+const jane = {
+  ...john,
+  name: 'Jane',
+  hobbies: john.hobbies.concat('MuayThai', 'Programming'),
+}
+
+
+
+
+/* jane.name = 'Jane'
+jane.hobbies.push('MuayThai', 'Programming') */
+
+/* console.log('John:', john)
+console.log('Jane:', jane)
+ */
